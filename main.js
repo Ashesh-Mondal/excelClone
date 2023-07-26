76cb15b967cd88a31d234c5625caf5d2a57ef9ba
ashesh-mondal <asheshmondal84@gmail.com>
2023-07-26 18:00:29 +0530

    cell structuring complete

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let rows = 100;
 let cols = 26;
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
 
 for (let i = 0; i < rows; i++) {
   let addressCol = document.createElement("div");
@@ -15,4 +16,15 @@ for (let i = 0; i < cols; i++) {
   addressRow.setAttribute("class", "address-row");
   addressRow.innerText = String.fromCharCode(i + 65);
   addressRowCont.appendChild(addressRow);
+}
+
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    rowCont.appendChild(cell);
+  }
+  cellsCont.appendChild(rowCont);
 }
