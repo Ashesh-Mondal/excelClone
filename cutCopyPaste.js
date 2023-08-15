a0f52fc262c37d2c16f6ca72cfe7533ac5da2bc3
ashesh-mondal <asheshmondal84@gmail.com>
2023-08-15 11:12:36 +0530

    Coding for the range selection of the cell to perform cut, copy and paste

--- cutCopyPaste.js
+++ cutCopyPaste.js
@@ -17,5 +17,26 @@ let rangeStorage = [];
 function handleSelectedCell(cell) {
   cell.addEventListener("click", (e) => {
     // Select cells range work
+    if (!altKey) return;
+    if (rangeStorage.length >= 2) {
+      defaultSelectedCellsUI();
+      rangeStorage = [];
+    }
+
+    // UI
+    cell.style.border = "2px solid #218c74";
+
+    let rid = Number(cell.getAttribute("rid"));
+    let cid = Number(cell.getAttribute("cid"));
+    rangeStorage.push([rid, cid]);
   });
+}
+
+function defaultSelectedCellsUI() {
+  for (let i = 0; i < rangeStorage.length; i++) {
+    let cell = document.querySelector(
+      `.cell[rid = "${rangeStorage[i][0]}"][cid = "${rangeStorage[i][1]}"]`
+    );
+    cell.style.border = "1px solid lightgrey";
+  }
 }
